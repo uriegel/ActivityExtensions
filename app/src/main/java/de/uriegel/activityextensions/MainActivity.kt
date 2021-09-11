13dbb8085bde9d74099aeaf22950e9b0affa9ce9
uriegel <uriegel@hotmail.de>
2021-09-11 12:30:38 +0200

    checkAndRequestPermissions

--- app/src/main/java/de/uriegel/activityextensions/MainActivity.kt
+++ app/src/main/java/de/uriegel/activityextensions/MainActivity.kt
@@ -1,9 +1,12 @@
 package de.uriegel.activityextensions
 
+import android.Manifest
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import de.uriegel.activityextensions.async.delay
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,6 +24,24 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
             launch {
                 val result = activityRequest.launch(Intent(this@MainActivity, TestActivity::class.java))
                 Toast.makeText(this@MainActivity, "Ergebnis: ${result.resultCode}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        permissions.setOnClickListener {
+            launch {
+                val result = activityRequest.checkAndAccessPermissions(arrayOf(
+                    Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ))
+                Toast.makeText(this@MainActivity, "Ergebnis: ${result.toString()}", Toast.LENGTH_LONG).show()
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                    delay(15000)
+
+                    val result2 =
+                        activityRequest.checkAndAccessPermissions(arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION))
+                    Toast.makeText(this@MainActivity,  "Ergebnis: ${result2.toString()}", Toast.LENGTH_LONG ).show()
+                }
             }
         }
     }
--- extensionslibrary/src/main/java/de/uriegel/activityextensions/ActivityRequest.kt
+++ extensionslibrary/src/main/java/de/uriegel/activityextensions/ActivityRequest.kt
@@ -1,10 +1,14 @@
 package de.uriegel.activityextensions
 
+import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import androidx.activity.ComponentActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.resume
 import kotlin.coroutines.suspendCoroutine
@@ -18,12 +22,33 @@ class ActivityRequest(activity: ComponentActivity) {
         }
     }
 
+    suspend fun checkAndAccessPermissions(permissions: Array<String>): Map<String, Boolean> {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return suspendCoroutine { continuation ->
+                if (permissions.any { context.checkSelfPermission(it) != PackageManager.PERMISSION_GRANTED }) {
+                    permissionsContinuation = continuation
+                    requestPermissionStarter.launch(permissions)
+                } else
+                    continuation.resume(permissions.map { it to true }.toMap())
+            }
+        } else
+            throw Exception("SDK 23 is required")
+    }
+
     private val starter: ActivityResultLauncher<Intent>
+    private val requestPermissionStarter: ActivityResultLauncher<Array<String>>
+    private val context: Context
     private lateinit var continuation: Continuation<ActivityResult>
+    private lateinit var permissionsContinuation: Continuation<Map<String, Boolean>>
 
     init {
         starter = activity.registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
             continuation.resume(it)
         }
+        requestPermissionStarter = activity.registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) {
+            permissionsContinuation.resume(it)
+        }
+
+        context = activity
     }
 }
